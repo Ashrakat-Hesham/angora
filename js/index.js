196c9e495b380967269732b26f3d31c71492eaa7
Ashrakat-Hesham <ashrakathesham2018@gmail.com>
2023-07-28 09:53:30 +0300

    upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
